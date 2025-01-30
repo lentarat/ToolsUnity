09d37aa79f60a1d499eee3a6358cc88120d92a20
lentar <lntr.exe@gmail.com>
2025-01-30 13:49:17 +0200

    DebugConsole Update

---
+++ Assets/DifferentScreensViewportFitter/readme.txt
@@ -0,0 +1,3 @@
+1. Choose desired phone screen.
+2. Expand DifferentScreensViewportFitter to the screen size. For easier adjustement you can enable SpriteRenderer component.
+3. Make selected gameObjects children to DifferentScreensViewportFitter.
---
+++ Assets/_Scripts/TestScript.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using LntrUtilities;
+
+public class TestScript : MonoBehaviour
+{
+    private void Awake()
+    {
+        DebugConsole.Instance.Log("Test");
+    }
+}
